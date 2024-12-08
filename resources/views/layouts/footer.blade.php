954b03bbde7c1728f042e07a03d64a5b3706ffbb
Husnain Shah <husnainamin44@gmail.com>
2024-12-08 12:31:18 +0500

    initial

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="bg-white sticky-footer">
+    <div class="container my-auto">
+        <div class="my-auto text-center copyright">
+            <span>Copyright © Your Website 2021</span>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,42 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-laugh-wink"></i>
+        </div>
+        <div class="mx-3 sidebar-brand-text">SB Admin <sup>2</sup></div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="my-0 sidebar-divider">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('dashboard') }}">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('products') }}">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Product</span></a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="/profile">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Profile</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="border-0 rounded-circle" id="sidebarToggle"></button>
+    </div>
+
+
+</ul>
